0e1e6174f9fac817139d68d7f74e397d42e76ac8
Eugene Azarov <rusjaz@gmail.com>
2025-12-29 15:23:37 +0300

    rtc bugfix

--- usb/rtc.c
+++ usb/rtc.c
@@ -4,7 +4,7 @@
 // interface for RTC device drivers
 //
 
-#include "rtc.h"
+#include "usb/rtc.h"
 
 uint8_t usb_rtc_get_time(uint8_t *date)
 {
@@ -13,7 +13,7 @@ uint8_t usb_rtc_get_time(uint8_t *date)
 
     if (dev)
     {
-        usb_rtc_class_config_t *rtc = (usb_rtc_class_config_t *) dev;
+        usb_rtc_class_config_t *rtc = (usb_rtc_class_config_t *) dev->class;
         return rtc->get_time(dev, date);
     }
 
@@ -27,7 +27,7 @@ uint8_t usb_rtc_set_time(uint8_t *date)
 
     if (dev)
     {
-        usb_rtc_class_config_t *rtc = (usb_rtc_class_config_t *) dev;
+        usb_rtc_class_config_t *rtc = (usb_rtc_class_config_t *) dev->class;
         return rtc->set_time(dev, date);
     }
 
